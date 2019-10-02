ef05fc5565bfa04dced3b84b0dde3e7d8ee65ddc
Steven Scott <stevescott2653@gmail.com>
2019-10-02 17:07:00 -0400

    Add files via upload

---
+++ Stack_Example/Stack_Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Stack_Example
+{
+    public class StackExample
+    {
+        public static void Main(string[] args)
+        {
+            Stack<string> names = new Stack<string>();
+            names.Push("John");
+            names.Push("Bob");
+            names.Push("Tom");
+            names.Push("Chris");
+            names.Push("Albert");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Peek element: " + names.Peek());
+            Console.WriteLine("Pop: " + names.Pop());
+            Console.WriteLine("After Pop, Peek element: " + names.Peek());
+        }
+    }
+}
